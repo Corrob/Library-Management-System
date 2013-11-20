e758c0689d186be63fccb0276f5d089875d12a7a
Corrob <corrob7@hotmail.com>
2013-11-20 13:11:52 -0700

    Fix bug where there are multiple event handlers on checkout and return

--- public/js/customer.js
+++ public/js/customer.js
@@ -368,6 +368,7 @@ var printBookData = function(data) {
           if (data[book]["avail_copies"] == 0) {
             // Do stuff!
           } else {
+            $("#bookShelf").off("click", "#checkoutBook" + data[book]["isbn"]);
             $("#bookShelf").on("click", "#checkoutBook" + data[book]["isbn"],
               checkoutHandlerMaker(data[book]["isbn"]));
           }
@@ -375,6 +376,7 @@ var printBookData = function(data) {
           content += "<button id='returnBook" + data[book]["isbn"]
                   + "' class='optionButtons'>Return"
                   + "</button>";
+          $("#bookShelf").off("click", "#returnBook" + data[book]["isbn"]);
           $("#bookShelf").on("click", "#returnBook" + data[book]["isbn"],
             returnHandlerMaker(data[book]["isbn"]));
         }
